5fbd5f5cb25ebb8faa9616eccc3410b9379548c6
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-08-02 14:09:08 -0300

    att

--- app.js
+++ app.js
@@ -1,165 +1,184 @@
-/* imports */
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-
-// Models
-const User = require('./models/User')
+const User = require('./models/User');
 
 app.get('/', (req, res) => {
-    res.status(200).json({msg: "Bem vindo a nossa api"})
-})
+    res.status(200).json({ msg: "Bem vindo a nossa API" });
+});
 
-// Private Route
-app.get("/user/:id", checkToken, async (req, res) => {
-    const id = req.params.id
-    // check if user exists
-    const user = await User.findById(id, '-password') // -password passado como segundo parametro para que se ele achar esse id ele nao retorne para const user a senha do usuario
-
-    if (!user) {
-        return res.status(404).json({msg: "user not found"})
-    }
-
-    res.status(200).json({ user })
-})
-
-function checkToken (req, res, next) {
-
-    const authHeader = req.headers['authorization'] // atuhorization e o nosso token que estamos pegando do header
-    const token = authHeader && authHeader.split(" ")[1] // aqui o token vem em um formato "bearer ..token" - precisamos apenas do token então damos um split para tirar os espaços, e o split tranforma em um array, pegamos [1] posição no caso seria o token apenas
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) {
-        return res.status(401).json({msg: "acesso negado"})
+        return res.status(401).json({ msg: "Acesso negado" });
     }
 
     try {
-        const secret = process.env.SECRET
-
-        jwt.verify(token, secret)
-        
-        next()
+        const secret = process.env.SECRET;
+        jwt.verify(token, secret);
+        next();
     } catch (error) {
-        return res.status(400).json({msg: "token invalido"})
+        return res.status(400).json({ msg: "Token inválido" });
     }
-
 }
 
-
-// Register User
-
 app.post('/auth/register', async (req, res) => {
+    const { name, email, password, confirmpassword } = req.body;
 
-    const {name, email, password, confirmpassword} = req.body;
-
-    // validations
-
-    if(!name) {
-        return res.status(422).json({msg: "The name is required"})
+    if (!name) {
+        return res.status(422).json({ msg: "O nome é obrigatório" });
     }
 
-    if(!email) {
-        return res.status(422).json({msg: "The email is required"})
+    if (!email) {
+        return res.status(422).json({ msg: "O e-mail é obrigatório" });
     }
 
-    if(!password) {
-        return res.status(422).json({msg: "The password is required"})
+    if (!password) {
+        return res.status(422).json({ msg: "A senha é obrigatória" });
     }
 
-    if(password !== confirmpassword) {
-        return res.status(422).json({msg: "Passwords must be the same"})
+    if (password !== confirmpassword) {
+        return res.status(422).json({ msg: "As senhas não conferem" });
     }
 
-    //check if user exists
-
-    const userExists = await User.findOne({ email: email })
+    const userExists = await User.findOne({ email });
 
     if (userExists) {
-        return res.status(422).json({msg: "User Exists, please try again with other email"})
+        return res.status(422).json({ msg: "E-mail já cadastrado, por favor use outro" });
     }
 
-    // create password
-
-    const salt = await bcrypt.genSalt(12)
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    // create user
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
 
     const user = new User({
         name,
         email,
         password: passwordHash,
-    })
+    });
 
     try {
-
-        await user.save()
-
-        res.status(201).json({msg: 'User created with success!'})
-
-
+        await user.save();
+        res.status(201).json({ msg: 'Usuário criado com sucesso!', user });
     } catch (error) {
-        console.log(error)
-        res.status(500).json({msg : 'happen error in the server, try again later'})
+        console.log(error);
+        res.status(500).json({ msg: 'Ocorreu um erro no servidor, tente novamente mais tarde' });
     }
-})
-
-// login User 
+});
 
 app.post('/auth/login', async (req, res) => {
-    const {email, password} = req.body;
-    if(!email) {
-        return res.status(422).json({msg: "The email is required"})
-    }
-    if(!password) {
-        return res.status(422).json({msg: "The password is required"})
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(422).json({ msg: "O e-mail é obrigatório" });
     }
 
-    const user = await User.findOne({ email: email })
-    if (!user) {
-        return res.status(404).json({msg: "User not Found, please try again with other email"})
+    if (!password) {
+        return res.status(422).json({ msg: "A senha é obrigatória" });
     }
-    // check if password match
-    const checkPassword = await bcrypt.compare(password, user.password)
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado, por favor tente outro e-mail" });
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+
     if (!checkPassword) {
-        return res.status(422).json({msg: "Password invalid, please try again with other password"})
+        return res.status(422).json({ msg: "Senha inválida, por favor tente novamente" });
     }
 
     try {
-        
-        const secret = process.env.SECRET
-
-        const token = jwt.sign({
-            id: user._id
-        }, secret,
-        )
-
-        res.status(200).json({ msg: "auth realized with success", token })
-
+        const secret = process.env.SECRET;
+        const token = jwt.sign({ id: user._id }, secret);
+        res.status(200).json({ msg: "Autenticação realizada com sucesso", token });
     } catch (error) {
-        console.log(error)
-        res.status(500).json({msg : 'happen error in the server, try again later'})
+        console.log(error);
+        res.status(500).json({ msg: 'Ocorreu um erro no servidor, tente novamente mais tarde' });
+    }
+});
+
+app.get("/user/:id", checkToken, async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findById(id, '-password');
+
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado" });
     }
 
-})
+    res.status(200).json({ user });
+});
 
+app.put('/user/:id', checkToken, async (req, res) => {
+    const id = req.params.id;
+    const { name, email, password, confirmpassword } = req.body;
 
-// Credencials
+    if (!name) {
+        return res.status(422).json({ msg: "O nome é obrigatório" });
+    }
 
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
+    if (!email) {
+        return res.status(422).json({ msg: "O e-mail é obrigatório" });
+    }
 
+    if (!password) {
+        return res.status(422).json({ msg: "A senha é obrigatória" });
+    }
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@mern-food-ordering-app.cljl3ik.mongodb.net/?retryWrites=true&w=majority&appName=mern-food-ordering-app`).then(() => {
-    app.listen(3000);
-    console.log("conectou ao banco!")
-}).catch((err) => console.log(err))
+    if (password !== confirmpassword) {
+        return res.status(422).json({ msg: "As senhas não conferem" });
+    }
 
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, { name, email, password: passwordHash }, { new: true });
+
+        if (!updatedUser) {
+            return res.status(404).json({ msg: "Usuário não encontrado" });
+        }
+
+        res.status(200).json({ msg: 'Usuário atualizado com sucesso!', updatedUser });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Ocorreu um erro no servidor, tente novamente mais tarde' });
+    }
+});
+
+app.delete('/user/:id', checkToken, async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const user = await User.findByIdAndDelete(id);
+
+        if (!user) {
+            return res.status(404).json({ msg: "Usuário não encontrado" });
+        }
+
+        res.status(200).json({ msg: 'Usuário deletado com sucesso!' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: 'Ocorreu um erro no servidor, tente novamente mais tarde' });
+    }
+});
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@mern-food-ordering-app.cljl3ik.mongodb.net/node-jwt-crud?retryWrites=true&w=majority&appName=mern-food-ordering-app`)
+    .then(() => {
+        app.listen(3000);
+        console.log("Conectado ao banco!");
+    })
+    .catch((err) => console.log(err));
